b2ddc9499dc83795063ce89093bb26c2694f1081
Russell Propert <russellpropert@gmail.com>
2022-01-15 12:31:10 -0500

    Hard coding the href link because of a build issue.

--- frontend/pages/success.js
+++ frontend/pages/success.js
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
 
-export default function Success({ message, button, link }) {
+export default function Success({ message, button }) {
 
   return (
     <main className='main'>
       <h1 className="title">Success</h1>
       <p className="description">{message}</p>
-      <Link href={link}>
+      <Link href="/">
         <a>
           <Button>{button}</Button>
         </a>
